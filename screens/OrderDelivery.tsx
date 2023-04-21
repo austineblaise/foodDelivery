2081d2306a24bc89d726bdc77f2eca7cb8429609
austine-blaise <austineblaise@gmail.com>
2023-04-21 16:44:46 +0100

    first commit

---
+++ screens/OrderDelivery.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const OrderDelivery = () => {
+  return (
+    <Text>OrderDelivery</Text>
+  )
+}
+
+export default OrderDelivery
---
+++ shims.d.ts
@@ -0,0 +1,4 @@
+declare module "react-native-vector-icons/Ionicons"
+declare module "@react-navigation/drawer"
+declare module "@react-navigation/native"
+declare module "@react-navigation/stack"
